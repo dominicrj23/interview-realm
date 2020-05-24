35cad0521148bc46415cab9c02e75234714d34df
Ashish Ranjan <ashish.ranjan.cse@gmail.com>
2020-05-24 12:16:20 +0200

    add a header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Header from './component/Header';
 
 function App() {
-    return <div />;
+    return (
+        <section className='todoapp'>
+            <Header />
+        </section>
+    );
 }
 
 export default App;
---
+++ src/component/Header.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+export const Header: FC = () => {
+    return (
+        <header className='header'>
+            <h1>todos</h1>
+        </header>
+    );
+};
+
+export default Header;
