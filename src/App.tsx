7f3552afaf2838f83d2d981f3cca89008b5e670b
Ashish Ranjan <ashish.ranjan.cse@gmail.com>
2020-05-24 12:16:20 +0200

    dispatch init when application is wired up

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
 import Header from './component/Header';
 import Footer from './component/Footer';
 import TodoList from './component/TodoList';
+import context from './context';
+import { INIT } from './actions';
 
 function App() {
+    const dispatch = context.useDispatchContext();
+
+    useEffect(() => {
+        dispatch({ type: INIT });
+    }, [dispatch]);
+
     return (
         <section className='todoapp'>
             <Header />
