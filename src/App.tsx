340b637ace0a2981894d409fc48212ba69c166c1
Ashish Ranjan <ashish.ranjan.cse@gmail.com>
2020-05-24 12:16:20 +0200

    add empty todo-list

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from './component/Header';
 import Footer from './component/Footer';
+import TodoList from './component/TodoList';
 
 function App() {
     return (
         <section className='todoapp'>
             <Header />
+            <section className='main'>
+                <TodoList />
+            </section>
             <Footer />
         </section>
     );
---
+++ src/component/TodoList.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+export const TodoList: FC = () => {
+    return <ul className='todo-list' />;
+};
+
+export default TodoList;
