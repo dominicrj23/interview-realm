f9e4a7c2613156afb0b5893fee294d3cf67903df
Ashish Ranjan <ashish.ranjan.cse@gmail.com>
2020-05-24 12:16:20 +0200

    push changes from state to local storage when state is updated

--- src/localStorageMiddleware.js
+++ src/localStorageMiddleware.js
@@ -1,10 +1,11 @@
-import { INIT, REHYDRATE } from './actions';
+import { INIT, REHYDRATE, ADD, TOGGLE, UPDATE } from './actions';
 
 const localStorageKey = 'todo-app';
+const updateActions = [ADD, TOGGLE, UPDATE];
 
 export default store => next => action => {
-    next(action);
     if (action.type === INIT) {
+        next(action);
         const rawValue = localStorage.getItem(localStorageKey);
         if (rawValue) {
             const value = JSON.parse(rawValue);
@@ -17,5 +18,13 @@ export default store => next => action => {
                 }
             });
         }
+        return;
     }
+    if (updateActions.includes(action.type)) {
+        next(action);
+        const state = store.getState();
+        localStorage.setItem(localStorageKey, JSON.stringify(state));
+        return;
+    }
+    next(action);
 };
