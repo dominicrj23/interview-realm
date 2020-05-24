cac2e52193e5d06294a2e312603a363663372857
Ashish Ranjan <ashish.ranjan.cse@gmail.com>
2020-05-24 12:16:20 +0200

    add body-parser

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const logger = require('morgan');
 const databaseMiddleware = require('./databaseMiddleware');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(logger('dev'));
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(databaseMiddleware);
 
 const emptyRoute = express();
