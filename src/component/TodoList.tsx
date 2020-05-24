33233d5140ad6a0d5ba7df50fa143019c478d3ca
Ashish Ranjan <ashish.ranjan.cse@gmail.com>
2020-05-24 12:16:20 +0200

    wire up todos from context

--- src/component/TodoList.tsx
+++ src/component/TodoList.tsx
@@ -1,15 +1,13 @@
 import React, { FC } from 'react';
 import clsx from 'clsx';
 import { TodoItem } from './TodoItem';
-
-const todos = [
-    {
-        text: 'Sample text for todo',
-        completed: false
-    }
-];
+import context from '../context';
 
 export const TodoList: FC = () => {
+    const todoOb = context.useStateContext();
+    //@ts-ignore
+    const todos = Object.keys(todoOb).map(k => todoOb[k]);
+
     return (
         <ul className={clsx('todo-list', { empty: todos.length === 0 })}>
             {todos.map(todo => (
