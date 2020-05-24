118d197cff080ad3a71dda908043590f23c7bf0c
Ashish Ranjan <ashish.ranjan.cse@gmail.com>
2020-05-24 12:16:20 +0200

    add reducer for SERVER actions

--- src/tasksReducer.js
+++ src/tasksReducer.js
@@ -1,10 +1,35 @@
-import { TODO, LOCALSTORAGE } from './actions';
+import { TODO, LOCALSTORAGE, SERVER } from './actions';
 export default (state = {}, { type, payload }) => {
     console.log(type, payload);
 
     switch (type) {
         case LOCALSTORAGE.REHYDRATE: {
             return payload.value;
+        }
+        case SERVER.REHYDRATE: {
+            const newState = payload.value.reduce(
+                (st, todo) => ({
+                    ...st,
+                    [todo.id]: todo
+                }),
+                {}
+            );
+
+            return newState;
+        }
+        case SERVER.UPDATE: {
+            return {
+                ...state,
+                [payload.id]: payload
+            };
+        }
+        case SERVER.CREATED: {
+            const { oldId, ...todo } = payload;
+            const { [oldId]: oldTodo, ...newState } = state;
+            return {
+                ...newState,
+                [todo.id]: todo
+            };
         }
         case TODO.ADD: {
             const { id, text } = payload;
