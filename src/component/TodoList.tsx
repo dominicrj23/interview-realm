6dd4a6cd81af95f1f19b02e049971977ab5912a4
Ashish Ranjan <ashish.ranjan.cse@gmail.com>
2020-05-24 12:16:20 +0200

    add content for empty todo-list

--- src/component/TodoList.tsx
+++ src/component/TodoList.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
 
 export const TodoList: FC = () => {
-    return <ul className='todo-list' />;
+    return <ul className='todo-list empty' />;
 };
 
 export default TodoList;
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 import 'todomvc-app-css/index.css';
+import './index.css';
 
 ReactDOM.render(
     <React.StrictMode>
