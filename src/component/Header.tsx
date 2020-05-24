543dc1aedf2de5775177648e80529a6539c76560
Ashish Ranjan <ashish.ranjan.cse@gmail.com>
2020-05-24 12:16:20 +0200

    add input field to the header

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -4,6 +4,11 @@ export const Header: FC = () => {
     return (
         <header className='header'>
             <h1>todos</h1>
+            <input
+                className='new-todo'
+                placeholder='Add text for new todo'
+                autoFocus
+            />
         </header>
     );
 };
