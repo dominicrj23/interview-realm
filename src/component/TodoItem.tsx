98ae7a1721b1f7472f9a22b260cf9c71bd4d6414
Ashish Ranjan <ashish.ranjan.cse@gmail.com>
2020-05-24 12:16:20 +0200

    add edit icon in todo item

--- src/component/TodoItem.tsx
+++ src/component/TodoItem.tsx
@@ -1,20 +1,37 @@
-import React, { FC } from 'react';
+import React, {
+    FC,
+    useState,
+    useCallback,
+    Dispatch,
+    SetStateAction
+} from 'react';
 import clsx from 'clsx';
 
 type TodoItemProp = {
     text: string;
     completed: boolean;
 };
+
+const toggle = (setStateCallback: Dispatch<SetStateAction<boolean>>) => () =>
+    setStateCallback(state => !state);
+
 export const TodoItem: FC<TodoItemProp> = ({ text, completed }) => {
+    const [editing, setEditing] = useState(false);
+
+    const toggleEditing = useCallback(toggle(setEditing), []);
+
     return (
         <li
             className={clsx({
-                completed
+                completed,
+                editing
             })}>
             <div className='view'>
+                <button className='edit-text' onClick={toggleEditing}></button>
                 <input className='toggle' type='checkbox' checked={completed} />
                 <label>{text}</label>
             </div>
+            <input className='edit' onBlur={toggleEditing} />
         </li>
     );
 };
