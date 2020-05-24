4dbfbe93245ed6c2f75b8b513f11e94c58539568
Ashish Ranjan <ashish.ranjan.cse@gmail.com>
2020-05-24 12:16:20 +0200

    add context with empty reducer and middleware

---
+++ src/context.js
@@ -0,0 +1,36 @@
+import React, { createContext, useContext } from 'react';
+import createReducer from 'react-use/lib/createReducer';
+
+const reducer = (state, action) => state;
+
+const logger = store => next => action => {
+    next(action);
+};
+
+export const generateContext = (reducer, middlewares) => {
+    const useReducer = createReducer(...middlewares);
+
+    const AppStateContext = createContext({});
+    const AppDispatchContext = createContext();
+
+    const Provider = ({ children }) => {
+        const [state, dispatch] = useReducer(reducer, {});
+        return (
+            <AppStateContext.Provider value={state}>
+                <AppDispatchContext.Provider value={dispatch}>
+                    {children}
+                </AppDispatchContext.Provider>
+            </AppStateContext.Provider>
+        );
+    };
+
+    const useStateContext = () => {
+        return useContext(AppStateContext);
+    };
+    const useDispatchContext = () => {
+        return useContext(AppDispatchContext);
+    };
+    return { useStateContext, useDispatchContext, Provider };
+};
+
+export default generateContext(reducer, [logger]);
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import Context from './context';
 import 'todomvc-app-css/index.css';
 import './index.css';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Context.Provider>
+            <App />
+        </Context.Provider>
     </React.StrictMode>,
     document.getElementById('root')
 );
