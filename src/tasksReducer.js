6fd5b5dd16b7374991e3fde3da0c4331700a2d0e
Ashish Ranjan <ashish.ranjan.cse@gmail.com>
2020-05-24 12:16:20 +0200

    add reducers for actions

--- src/tasksReducer.js
+++ src/tasksReducer.js
@@ -1,11 +1,45 @@
-import { REHYDRATE } from './actions';
-
+import { REHYDRATE, ADD, TOGGLE, UPDATE } from './actions';
+import { v4 as uuid } from 'uuid';
 export default (state = {}, { type, payload }) => {
     console.log(type, payload);
 
     switch (type) {
         case REHYDRATE: {
-            return payload.value;
+            if (payload.source === 'localStorage') {
+                return payload.value;
+            }
+            return state;
+        }
+        case ADD: {
+            const id = uuid();
+            return {
+                ...state,
+                [id]: {
+                    id,
+                    completed: false,
+                    text: payload.text
+                }
+            };
+        }
+        case TOGGLE: {
+            const { id } = payload;
+            return {
+                ...state,
+                [id]: {
+                    ...state[id],
+                    completed: !state[id].completed
+                }
+            };
+        }
+        case UPDATE: {
+            const { id, ...value } = payload;
+            return {
+                ...state,
+                [id]: {
+                    ...state[id],
+                    ...value
+                }
+            };
         }
         default:
             return state;
