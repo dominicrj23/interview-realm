b00b58c8d0f1e7ca5835d9c8d17a5e6986093da3
Ashish Ranjan <ashish.ranjan.cse@gmail.com>
2020-05-24 12:16:20 +0200

    dispatch TOGGLE event when task is toggled

--- src/actions.ts
+++ src/actions.ts
@@ -1,3 +1,4 @@
 export const INIT = '@@app/init';
 export const REHYDRATE = '@@app/rehydrate';
+export const TOGGLE = '@@ITEM/toggle';
 export const ADD = '@@ITEM/add';
--- src/component/TodoItem.tsx
+++ src/component/TodoItem.tsx
@@ -6,8 +6,10 @@ import React, {
     SetStateAction
 } from 'react';
 import clsx from 'clsx';
-
+import { TOGGLE } from '../actions';
+import context from '../context';
 type TodoItemProp = {
+    id: string;
     text: string;
     completed: boolean;
 };
@@ -15,11 +17,15 @@ type TodoItemProp = {
 const toggle = (setStateCallback: Dispatch<SetStateAction<boolean>>) => () =>
     setStateCallback(state => !state);
 
-export const TodoItem: FC<TodoItemProp> = ({ text, completed }) => {
+export const TodoItem: FC<TodoItemProp> = ({ id, text, completed }) => {
     const [editing, setEditing] = useState(false);
+    const dispatch = context.useDispatchContext();
 
     const toggleEditing = useCallback(toggle(setEditing), []);
 
+    const toggleCompleted = useCallback(() => {
+        dispatch({ type: TOGGLE, payload: { id } });
+    }, [dispatch, id]);
     return (
         <li
             className={clsx({
@@ -28,7 +34,12 @@ export const TodoItem: FC<TodoItemProp> = ({ text, completed }) => {
             })}>
             <div className='view'>
                 <button className='edit-text' onClick={toggleEditing}></button>
-                <input className='toggle' type='checkbox' checked={completed} />
+                <input
+                    className='toggle'
+                    type='checkbox'
+                    checked={completed}
+                    onChange={toggleCompleted}
+                />
                 <label>{text}</label>
             </div>
             <input className='edit' onBlur={toggleEditing} />
