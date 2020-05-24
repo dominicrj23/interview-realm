a365954c9ed6c0c45299d498d5a280d1a8a3b59d
Ashish Ranjan <ashish.ranjan.cse@gmail.com>
2020-05-24 12:16:20 +0200

    [ListItem]: add toggle button to show completed status

--- src/component/TodoItem.tsx
+++ src/component/TodoItem.tsx
@@ -1,12 +1,18 @@
 import React, { FC } from 'react';
+import clsx from 'clsx';
 
 type TodoItemProp = {
     text: string;
+    completed: boolean;
 };
-export const TodoItem: FC<TodoItemProp> = ({ text }) => {
+export const TodoItem: FC<TodoItemProp> = ({ text, completed }) => {
     return (
-        <li>
+        <li
+            className={clsx({
+                completed
+            })}>
             <div className='view'>
+                <input className='toggle' type='checkbox' checked={completed} />
                 <label>{text}</label>
             </div>
         </li>
