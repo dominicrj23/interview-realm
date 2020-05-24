93b78781ce34c4dc8d3ce064d08123758dff1f93
Ashish Ranjan <ashish.ranjan.cse@gmail.com>
2020-05-24 12:16:20 +0200

    hydrate state from server on application init

--- src/serverMiddleware.js
+++ src/serverMiddleware.js
@@ -1,3 +1,25 @@
+import { INIT, REHYDRATE } from './actions';
+
+const getTodoIds = () => fetch('/api').then(res => res.json());
+const getTodo = id => fetch(`/api/${id}`).then(res => res.json());
+
 export default store => next => action => {
+    if (action.type === INIT) {
+        next(action);
+        getTodoIds().then(idArray => {
+            Promise.all(idArray.map(id => getTodo(id))).then(value => {
+                store.dispatch({
+                    type: REHYDRATE,
+                    payload: {
+                        source: 'server',
+                        value
+                    }
+                });
+            });
+        });
+
+        return;
+    }
+
     next(action);
 };
